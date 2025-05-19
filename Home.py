ffa9dc1e0694ee1b0b981409b01adaa6cf34cd2b
KY7437 <eugene7437@gmail.com>
2025-05-19 13:27:52 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+st.title("😚Group1")
+st.caption("This is a group project of G1")
+
+st.write("This should be a collaboratory work")
+st.write("一生懸命頑張っていこうね🎶")
+st.markdown("---")
