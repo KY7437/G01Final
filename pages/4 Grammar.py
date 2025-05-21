cede3205c5823036bbece08c39fea21333aa5dec
KY7437 <eugene7437@gmail.com>
2025-05-21 14:35:23 +0900

    Update 4 Grammar.py

--- pages/4 Grammar.py
+++ pages/4 Grammar.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+from gtts import gTTS
+from io import BytesIO
+import random
 
+st.write("🌱 Vocabulary learning")
+
+tab1, tab2, tab3, tab4 = st.tabs(["❄️ Read after TTS", "❄️ ", "❄️ "])
