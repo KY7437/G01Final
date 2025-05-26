fc4b681f92527b0295aebe0088f4f757e0789704
KY7437 <eugene7437@gmail.com>
2025-05-26 13:02:25 +0900

    Update 2 About.py

--- pages/2 About.py
+++ pages/2 About.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-st.write("In the end of this lesson,)
+st.write("In the end of this lesson,")
 
-         st.write(Students will be able to understand the passage.
-Students will be able to understand and apply the grammar rules.
-Students will be able to memorize nessasary words.
-Students will be able to ....")
+st.write("""
+- Students will be able to understand the passage.
+- Students will be able to understand and apply the grammar rules.
+- Students will be able to memorize necessary words.
+- Students will be able to ...
+""")
+
