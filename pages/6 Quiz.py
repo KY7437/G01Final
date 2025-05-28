d3a7c2435e5944ff9c8108f37c04290bae9ea7d2
KY7437 <eugene7437@gmail.com>
2025-05-28 14:54:16 +0900

    Update 6 Quiz.py

--- pages/6 Quiz.py
+++ pages/6 Quiz.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+from gtts import gTTS
+from io import BytesIO
+import random
+import tempfile
 
+st.write("🌱 Vocabulary learning")
+
+# Define tabs
+tab1, tab2, tab3 = st.tabs(["❄️ Level 1", "❄️ Level 2", "❄️ Level 3"]
