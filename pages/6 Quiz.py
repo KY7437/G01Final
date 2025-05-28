4b904674fbc593824ce392ad5272b671f5dde968
KY7437 <eugene7437@gmail.com>
2025-05-28 14:54:36 +0900

    Update 6 Quiz.py

--- pages/6 Quiz.py
+++ pages/6 Quiz.py
@@ -5,7 +5,7 @@ from io import BytesIO
 import random
 import tempfile
 
-st.write("🌱 Vocabulary learning")
+st.write("Review Check")
 
 # Define tabs
 tab1, tab2, tab3 = st.tabs(["❄️ Level 1", "❄️ Level 2", "❄️ Level 3"]
