5f97dd8f6046512b4e02b97e57d54a4b39860c30
KY7437 <eugene7437@gmail.com>
2025-06-02 14:21:05 +0900

    Update 7 📚Study Alone.py

--- pages/7 📚Study Alone.py
+++ pages/7 📚Study Alone.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from gtts import gTTS
+from io import BytesIO
+import re
 
+st.title("Passage Reading")
+
+# Define tabs
+tab1, tab2, tab3 = st.tabs(["Guildlines", "Memorization of the text", "a"])
+
+with tab1: 
+  st.title("Guidelines")
+
+with tab2:
+  st.title("Memorization of the text")
+
+with tab3:
+  st.title("a")
