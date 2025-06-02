537e43f1478d9f0289fd31f3fb9ccdda71d0a533
KY7437 <eugene7437@gmail.com>
2025-06-02 13:40:17 +0900

    Update 2 About.py

--- pages/2 About.py
+++ pages/2 About.py
@@ -3,9 +3,6 @@ import streamlit as st
 st.header("In the end of this lesson,")
 
 st.write("""
-- Students will be able to understand the passage.
-- Students will be able to understand and apply the grammar rules.
-- Students will be able to memorize necessary words.
-- Students will be able to ...
+app introduction video with lesson goals menu by menu
 """)
 
