635cd5a286de580ea0b26be14fe9c396f926e5a6
KY7437 <eugene7437@gmail.com>
2025-06-03 19:03:19 +0900

    Update 2 About.py

--- pages/2 About.py
+++ pages/2 About.py
@@ -6,6 +6,8 @@ st.markdown("""
 Welcome to the **Middle School English Learning App**, a comprehensive platform designed to help students improve their English skills through **interactive and engaging activities**.  
 This app integrates vocabulary, grammar, reading, and quizzes in one easy-to-use interface—perfect for **independent study** or **classroom support**.
 
+st.video(https://github.com/KY7437/G01Final/raw/main/data/dl.mp4, width=500)
+
 ---
 
 ### 🎯 App Goals
