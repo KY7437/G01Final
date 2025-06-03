d0b4e513d9ef9c118e9946f554174bf68c282f2e
KY7437 <eugene7437@gmail.com>
2025-06-03 17:13:07 +0900

    Update 2 About.py

--- pages/2 About.py
+++ pages/2 About.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("📘 About This App")
 
-st.video("https://github.com/KY7437/G01Final/raw/main/data/dl.mp4", caption="Introduction Video", width=500)
+st.video("https://github.com/KY7437/G01Final/blob/main/data/dl.mp4", caption="Introduction Video", width=500)
 
 st.markdown("""
 Welcome to the **Middle School English Learning App**, a comprehensive platform designed to help students improve their English skills through **interactive and engaging activities**.  
